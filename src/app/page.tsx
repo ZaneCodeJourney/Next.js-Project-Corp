5a98ead0abd9675fd13316f3c46718d68b61839e
Ziyang Wen <wenziyang007@gmail.com>
2024-01-25 12:52:42 +0800

    save and display some images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
-import Link from "next/link";
+import Image from "next/image";
+import homeImg from "public/home.jpg";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImg}
+          alt="car factory"
+          fill
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    </div>
+  );
 }
